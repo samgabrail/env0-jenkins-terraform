cc76f6af797001ff3a7e225f130267c6a353696e
Sam Gabrail <sam.gabrail@tekanaid.com>
2023-10-23 17:30:23 -0400

    updated TF, added ansible playbook and JenkinsFile

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.77.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
@@ -5,32 +14,6 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "MonitoringResources"
   location = "East US"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "MonitoringVNet"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "default"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "MonitoringNIC"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
@@ -53,15 +36,21 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   computer_name  = "monitoringvm"
   admin_username = "azureuser"
-  admin_password = "Password1234!"
+
+  // Add a public key to the same folder as the main.tf script (we use Ansible to send the private key to the Jenkins machine)
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("id_rsa.pub")
+  }
 
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
 
-  disable_password_authentication = false
+  disable_password_authentication = true
 }
 
+
 output "public_ip" {
-  value = azurerm_linux_virtual_machine.vm.public_ip_address
+  value = azurerm_public_ip.pip.ip_address
 }
---
+++ Terraform/networking.tf
@@ -0,0 +1,66 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "MonitoringVNet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "MonitoringNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+
+resource "azurerm_public_ip" "pip" {
+  name                = "MonitoringPublicIP"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+}
+
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "MonitoringNSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "rule" {
+  for_each = {
+    grafana    = { priority = 1001, port = 3000 }
+    prometheus = { priority = 1002, port = 9090 }
+    ssh        = { priority = 1003, port = 22 }
+  }
+
+  name                        = "${upper(each.key)}Rule"
+  priority                    = each.value.priority
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value.port
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
