f292e1c7664fe2510ca0151355ec419b6fe7ce70
Sam Gabrail <sam.gabrail@tekanaid.com>
2023-10-23 18:01:35 -0400

    updated playbook

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "MonitoringVM"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1ls"
+  size                = "Standard_B2s"
 
   os_disk {
     caching              = "ReadWrite"
